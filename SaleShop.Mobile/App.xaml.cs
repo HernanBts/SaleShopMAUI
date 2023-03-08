7f4eea09095ec1226550630ad7f2f1f77eb7927e
Hernanbts <bts.hernan@gmail.com>
2023-03-08 07:06:57 -0300

    First commit with project archicteture

---
+++ SaleShop.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SaleShop.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SaleShop.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SaleShop.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
