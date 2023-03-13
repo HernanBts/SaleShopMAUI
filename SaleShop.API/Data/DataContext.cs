66e9aebc0a8311a1f3620abd407f4d8155d574fd
Hernanbts <bts.hernan@gmail.com>
2023-03-13 17:16:07 -0300

    Add DataContext

---
+++ SaleShop.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaleShop.Shared.Entities;
+
+namespace SaleShop.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name);
+        }
+    }
+}
