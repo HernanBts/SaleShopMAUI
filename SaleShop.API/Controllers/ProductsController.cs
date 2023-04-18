53157628aad81d3a6e4b2d311df70a2e3e107a99
Hernanbts <bts.hernan@gmail.com>
2023-04-18 18:46:08 -0300

    add createproducts func

---
+++ SaleShop.API/Controllers/ProductsController.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SaleShop.API.Data;
+using SaleShop.API.Helpers;
+using SaleShop.Shared.DTOs;
+using SaleShop.Shared.Entities;
+
+namespace SaleShop.API.Controllers
+{
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("/api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IFileStorage _fileStorage;
+
+        public ProductsController(DataContext context, IFileStorage fileStorage)
+        {
+            _context = context;
+            _fileStorage = fileStorage;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get([FromQuery] PaginationDTO pagination)
+        {
+            var queryable = _context.Products
+                .Include(x => x.ProductImages)
+                .Include(x => x.ProductCategories)
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+            return Ok(await queryable
+                .OrderBy(x => x.Name)
+                .Paginate(pagination)
+                .ToListAsync());
+        }
+
+
+        [HttpGet("totalPages")]
+        public async Task<ActionResult> GetPages([FromQuery] PaginationDTO pagination)
+        {
+            var queryable = _context.Products
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+            double count = await queryable.CountAsync();
+            double totalPages = Math.Ceiling(count / pagination.ItemsNumber);
+            return Ok(totalPages);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var product = await _context.Products
+                .Include(x => x.ProductImages)
+                .Include(x => x.ProductCategories!)
+                .ThenInclude(x => x.Category)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(ProductDTO productDTO)
+        {
+            try
+            {
+                Product newProduct = new()
+                {
+                    Name = productDTO.Name,
+                    Description = productDTO.Description,
+                    Price = productDTO.Price,
+                    Stock = productDTO.Stock,
+                    ProductCategories = new List<ProductCategory>(),
+                    ProductImages = new List<ProductImage>()
+                };
+
+                foreach (var productImage in productDTO.ProductImages!)
+                {
+                    var photoProduct = Convert.FromBase64String(productImage);
+                    newProduct.ProductImages.Add(new ProductImage { Image = await _fileStorage.SaveFileAsync(photoProduct, ".jpg", "products") });
+                }
+
+                foreach (var productCategoryId in productDTO.ProductCategoryIds!)
+                {
+                    newProduct.ProductCategories.Add(new ProductCategory { Category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == productCategoryId) });
+                }
+
+                _context.Add(newProduct);
+                await _context.SaveChangesAsync();
+                return Ok(productDTO);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe una ciudad con el mismo nombre.");
+                }
+
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Product product)
+        {
+            try
+            {
+                _context.Update(product);
+                await _context.SaveChangesAsync();
+                return Ok(product);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un producto con el mismo nombre.");
+                }
+
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(product);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
+
---
+++ SaleShop.WEB/Helpers/MultipleSelectorModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SaleShop.WEB.Helpers
+{
+    public class MultipleSelectorModel
+    {
+        public MultipleSelectorModel(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
+
